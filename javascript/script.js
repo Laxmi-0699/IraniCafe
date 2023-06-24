e2890a1a931a7e0c9dbd2ab77adf4e59e7f76279
laxmiwalekar <laxmiwalekar3@gmail.com>
2023-06-24 21:17:34 +0530

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,431 @@
+
+let navbar = document.querySelector('.header.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.add('active');
+}
+
+
+
+document.querySelector('#close-navbar').onclick = () =>{
+  navbar.classList.remove('active');
+}
+
+
+
+
+        function fun()
+        {
+         
+        
+
+            var x = document.getElementById("frm1").value;
+            alert("Hi "+" " + x +  " Thank You For Reaching Us We will Connect You Soon");
+        
+        }
+
+
+        
+        
+    let addBtn = document.querySelector('#add');
+    let subBtn = document.querySelector('#sub');
+    let qty = document.querySelector('#qtyBox');
+    
+    addBtn.addEventListener('click',() =>{
+        qty.value = parseInt(qty.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn.addEventListener('click',() =>{
+      if (qty.value <= 0 ){
+        qty.value = 0;
+      }
+    else{
+        qty.value = parseInt(qty.value) - 1;
+    }
+    
+    });
+
+
+    
+    let addBtn1 = document.querySelector('#add1');
+    let subBtn1 = document.querySelector('#sub1');
+    let qty1 = document.querySelector('#qtyBox1');
+    
+    addBtn1.addEventListener('click',() =>{
+        qty1.value = parseInt(qty1.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn1.addEventListener('click',() =>{
+      if (qty1.value <= 0 ){
+        qty1.value = 0;
+      }
+    else{
+        qty1.value = parseInt(qty1.value) - 1;
+    }
+    
+    });
+        
+       
+    let addBtn2 = document.querySelector('#add2');
+    let subBtn2 = document.querySelector('#sub2');
+    let qty2 = document.querySelector('#qtyBox2');
+    
+    addBtn2.addEventListener('click',() =>{
+        qty2.value = parseInt(qty2.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn2.addEventListener('click',() =>{
+      if (qty2.value <= 0 ){
+        qty2.value = 0;
+      }
+    else{
+        qty2.value = parseInt(qty2.value) - 1;
+    }
+    
+    });
+
+       
+    let addBtn3 = document.querySelector('#add3');
+    let subBtn3 = document.querySelector('#sub3');
+    let qty3 = document.querySelector('#qtyBox3');
+    
+    addBtn3.addEventListener('click',() =>{
+        qty3.value = parseInt(qty3.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn3.addEventListener('click',() =>{
+      if (qty3.value <= 0 ){
+        qty3.value = 0;
+      }
+    else{
+        qty3.value = parseInt(qty3.value) - 1;
+    }
+    
+    });
+
+
+       
+    let addBtn4 = document.querySelector('#add4');
+    let subBtn4 = document.querySelector('#sub4');
+    let qty4 = document.querySelector('#qtyBox4');
+    
+    addBtn4.addEventListener('click',() =>{
+        qty4.value = parseInt(qty4.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn4.addEventListener('click',() =>{
+      if (qty4.value <= 0 ){
+        qty4.value = 0;
+      }
+    else{
+        qty4.value = parseInt(qty4.value) - 1;
+    }
+    
+    });
+
+
+       
+    let addBtn5 = document.querySelector('#add5');
+    let subBtn5 = document.querySelector('#sub5');
+    let qty5 = document.querySelector('#qtyBox5');
+    
+    addBtn5.addEventListener('click',() =>{
+        qty5.value = parseInt(qty5.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn5.addEventListener('click',() =>{
+      if (qty5.value <= 0 ){
+        qty5.value = 0;
+      }
+    else{
+        qty5.value = parseInt(qty5.value) - 1;
+    }
+    
+    });
+
+
+
+
+       
+    let addBtn6 = document.querySelector('#add6');
+    let subBtn6 = document.querySelector('#sub6');
+    let qty6 = document.querySelector('#qtyBox6');
+    
+    addBtn6.addEventListener('click',() =>{
+        qty6.value = parseInt(qty6.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn6.addEventListener('click',() =>{
+      if (qty6.value <= 0 ){
+        qty6.value = 0;
+      }
+    else{
+        qty6.value = parseInt(qty6.value) - 1;
+    }
+    
+    });
+
+
+
+
+       
+    let addBtn7 = document.querySelector('#add7');
+    let subBtn7 = document.querySelector('#sub7');
+    let qty7 = document.querySelector('#qtyBox7');
+    
+    addBtn7.addEventListener('click',() =>{
+        qty7.value = parseInt(qty7.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn7.addEventListener('click',() =>{
+      if (qty7.value <= 0 ){
+        qty7.value = 0;
+      }
+    else{
+        qty7.value = parseInt(qty7.value) - 1;
+    }
+    
+    });
+
+
+
+
+       
+    let addBtn8 = document.querySelector('#add8');
+    let subBtn8 = document.querySelector('#sub8');
+    let qty8 = document.querySelector('#qtyBox8');
+    
+    addBtn8.addEventListener('click',() =>{
+        qty8.value = parseInt(qty8.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn8.addEventListener('click',() =>{
+      if (qty8.value <= 0 ){
+        qty8.value = 0;
+      }
+    else{
+        qty8.value = parseInt(qty8.value) - 1;
+    }
+    
+    });
+
+
+
+       
+    let addBtn9 = document.querySelector('#add9');
+    let subBtn9 = document.querySelector('#sub9');
+    let qty9 = document.querySelector('#qtyBox9');
+    
+    addBtn9.addEventListener('click',() =>{
+        qty9.value = parseInt(qty9.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn9.addEventListener('click',() =>{
+      if (qty9.value <= 0 ){
+        qty9.value = 0;
+      }
+    else{
+        qty9.value = parseInt(qty9.value) - 1;
+    }
+    
+    });
+
+
+       
+    let addBtn10 = document.querySelector('#add10');
+    let subBtn10 = document.querySelector('#sub10');
+    let qty10 = document.querySelector('#qtyBox10');
+    
+    addBtn10.addEventListener('click',() =>{
+        qty10.value = parseInt(qty10.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn10.addEventListener('click',() =>{
+      if (qty10.value <= 0 ){
+        qty10.value = 0;
+      }
+    else{
+        qty10.value = parseInt(qty10.value) - 1;
+    }
+    
+    });
+
+
+       
+    let addBtn11 = document.querySelector('#add11');
+    let subBtn11 = document.querySelector('#sub11');
+    let qty11 = document.querySelector('#qtyBox11');
+    
+    addBtn11.addEventListener('click',() =>{
+        qty11.value = parseInt(qty11.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn11.addEventListener('click',() =>{
+      if (qty11.value <= 0 ){
+        qty11.value = 0;
+      }
+    else{
+        qty11.value = parseInt(qty11.value) - 1;
+    }
+    
+    });
+
+
+       
+    let addBtn12 = document.querySelector('#add12');
+    let subBtn12 = document.querySelector('#sub12');
+    let qty12 = document.querySelector('#qtyBox12');
+    
+    addBtn12.addEventListener('click',() =>{
+        qty12.value = parseInt(qty12.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn12.addEventListener('click',() =>{
+      if (qty12.value <= 0 ){
+        qty12.value = 0;
+      }
+    else{
+        qty12.value = parseInt(qty12.value) - 1;
+    }
+    
+    });
+
+
+
+       
+    let addBtn13 = document.querySelector('#add13');
+    let subBtn13 = document.querySelector('#sub13');
+    let qty13 = document.querySelector('#qtyBox13');
+    
+    addBtn13.addEventListener('click',() =>{
+        qty13.value = parseInt(qty13.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn13.addEventListener('click',() =>{
+      if (qty13.value <= 0 ){
+        qty13.value = 0;
+      }
+    else{
+        qty13.value = parseInt(qty13.value) - 1;
+    }
+    
+    });
+
+
+
+
+       
+    let addBtn14 = document.querySelector('#add14');
+    let subBtn14 = document.querySelector('#sub14');
+    let qty14 = document.querySelector('#qtyBox14');
+    
+    addBtn14.addEventListener('click',() =>{
+        qty14.value = parseInt(qty14.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn14.addEventListener('click',() =>{
+      if (qty14.value <= 0 ){
+        qty14.value = 0;
+      }
+    else{
+        qty14.value = parseInt(qty14.value) - 1;
+    }
+    
+    });
+
+
+
+
+
+       
+    let addBtn15 = document.querySelector('#add15');
+    let subBtn15 = document.querySelector('#sub15');
+    let qty15 = document.querySelector('#qtyBox15');
+    
+    addBtn15.addEventListener('click',() =>{
+        qty15.value = parseInt(qty15.value) + 1;
+    });
+    
+    
+    
+    
+    subBtn15.addEventListener('click',() =>{
+      if (qty15.value <= 0 ){
+        qty15.value = 0;
+      }
+    else{
+        qty15.value = parseInt(qty15.value) - 1;
+    }
+    
+    });
+        
+
+
+
+
+
+
+
+
+
+
+
+/*
+let navbar =document.querySelector('.header.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.add('active');
+}
+
+
+
+
+document.querySelector('#close-navbar').onclick = () => {
+    navbar.classList.remove('active');
+}*/
+
